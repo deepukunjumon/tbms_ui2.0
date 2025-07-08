0b7b5a26ef58459a6489fe532e26d1bc3ad1c639
Deepu Kunjumon <deepu.k@acumengroup.in>
2025-07-08 15:29:27 +0530

    Production API Url added

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 const API = axios.create({
-    baseURL: "http://localhost:8080/api",
+    baseURL: "https://api-tbms.up.railway.app/api",
 });
 
 export const API_ENDPOINTS = {
